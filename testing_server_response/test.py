5decc6c6f446ee5d08df31dd29ae9fbfe013257e
elizawetta <lari.x@mail.ru>
2022-03-08 15:24:13 +1000

    Update test.py

--- testing_server_response/test.py
+++ testing_server_response/test.py
@@ -1,10 +1,10 @@
 from requests import get
 
 # Получение всех работ
-print(get('http://localhost:5000/api/news').json())
+print(get('http://localhost:5000/api/jobs').json())
 # Корректное получение одной работы
-print(get('http://localhost:5000/api/news/1').json())
+print(get('http://localhost:5000/api/jobs/1').json())
 # Ошибочный запрос на получение одной работы — неверный id
-print(get('http://localhost:5000/api/news/999').json())
+print(get('http://localhost:5000/api/jobs/999').json())
 # Ошибочный запрос на получение одной работы — строка
-print(get('http://localhost:5000/api/news/ok').json())
+print(get('http://localhost:5000/api/jobs/ok').json())
